cc740a6d82f855277efccc41a6f8b79dbde5c198
b-alkhateeb <basil.y.alkhateeb@gmail.com>
2018-11-01 22:25:17 +0200

    add node IDs

--- sensor.c
+++ sensor.c
@@ -1,46 +1,46 @@
-
 #include  <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
+#define NODEID 2                        // Give a node ID
 
-// Used for the NRF24L01+
-int msg[1];
 RF24 radio(9,10);
-const uint64_t pipe = 0xE8E8F0F0E1LL;
-
-// For the Ultrasonic
+const uint64_t pipe = 0xE8E8F0F0E5LL;   // Change the last byte for each node
 const int trigPin = 3;
 const int echoPin = 5;
 long duration;
-int distance;
+int distance=5;
+int msg[2];
 
-void setup(void) {
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-
-  radio.begin();
-  radio.openWritingPipe(pipe);
+void setup(void){
+    pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+    pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+    Serial.begin(9600);
+    radio.begin();
+    radio.openWritingPipe(pipe);
 }
 
 
 void loop(void){
-
-  // read ultrasonic distance into the distance variable
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance= duration*0.034/2;
-  
-  // send the distance
-  msg[0] = distance;
-  int done = 0;
-  // keep trying to send, terrible evergy use, needs fixing
-  while (done == 0) {
-    done = radio.write(msg, 1);
-  }
-  
-  delay(1000);
+    // Ultrasonic reading //
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance= duration*0.034/2;
+    
+    // construct message //
+    msg[0] = NODEID;
+    msg[1] = distance;
+    
+    // send id and distance //
+    int done = 0;
+    while (done == 0) {
+        done = radio.write(msg, sizeof(msg));
+    }
+    
+    
+    delay(1000);
+    
 }
--- sink.c
+++ sink.c
@@ -1,32 +1,66 @@
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
+#include <Wire.h>
 
-int msg[1];
+#define GATEWAY_ADDR 9
+int msg[2];
 RF24 radio(9,10);
-const uint64_t pipe = 0xE8E8F0F0E1LL;
 
-void setup(void){
-  Serial.begin(9600);
 
-  radio.begin();
-  radio.openReadingPipe(1,pipe);
-  radio.startListening();
+// Add all node addresses
+const uint64_t pipe1 = 0xE8E8F0F0E1LL;
+const uint64_t pipe2 = 0xE8E8F0F0E5LL;
+
+
+// struct for I2C communication
+struct Reading{
+    float id;
+    float measurement;
+};
+
+
+
+void setup() {
+    Wire.begin(); // join i2c bus (address optional for master)
+    Serial.begin(9600);
+    
+    radio.begin();
+    radio.openReadingPipe(1,pipe1);
+    radio.openReadingPipe(2,pipe2);
+    radio.startListening();
 }
 
-void loop(void){
-  if (radio.available()){
-    int done = 0;    
-    while (done == 0){
-      done = radio.read(msg, 1);     
 
-      // display recieved data, mapping is done for my water container
-      Serial.print("Water Level: "); 
-      Serial.print(-5*(msg[0]-17));
-      Serial.println("%");
-      delay(10);
+void loop() {
+    // Recieve message //
+    if (radio.available()){
+        int done = 0;
+        while (done == 0){
+            done = radio.read(msg, sizeof(msg));
+            delay(10);
+        }
     }
-  }
-  
-  delay(1000);
+    
+    
+    // Create I2C structure //
+    Reading r;
+    r.id = float(msg[0]);
+    r.measurement = float(msg[1]);
+    
+    // Display on Serial Monitor for debugging //
+    Serial.print(r.id);
+    Serial.print(" ");
+    Serial.println(r.measurement);
+    
+    // send I2C //
+    sendReading(r);
+    delay(2000);
+}
+
+
+void sendReading(Reading r){
+    Wire.beginTransmission(GATEWAY_ADDR); // transmit to device #8
+    Wire.write((byte *) &r, sizeof(r));
+    Wire.endTransmission();    // stop transmitting
 }
